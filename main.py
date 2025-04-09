0372800507f6ac7aa14968068e96422cbf8031d1
Staritsin <staritsinar@gmail.com>
2025-04-09 18:51:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+import requests
+import os
+
+app = Flask(__name__)
+BOT_TOKEN = '8056198011:AAFZMYMpWGvOjhEucRhquBJ1Hoc0d8y0K6s'
+TELEGRAM_API_URL = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+
+@app.route('/')
+def index():
+    return 'Бот работает!'
+
+@app.route('/telegram', methods=['POST'])
+def telegram_webhook():
+    data = request.get_json()
+
+    if 'message' in data:
+        chat_id = data['message']['chat']['id']
+        if 'text' in data['message']:
+            text = data['message']['text']
+            reply = f"Ты написал: {text}"
+        elif 'voice' in data['message']:
+            reply = f"Я получил голосовое сообщение!"
+        elif 'photo' in data['message']:
+            reply = f"Фото получено!"
+        elif 'video' in data['message']:
+            reply = f"Видео получено!"
+        else:
+            reply = "Я пока не знаю, что с этим делать..."
+
+        requests.post(TELEGRAM_API_URL, json={
+            'chat_id': chat_id,
+            'text': reply
+        })
+
+    return 'ok'
+
+if __name__ == '__main__':
+    app.run(debug=True)

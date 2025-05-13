e206fe18f5051c0257463d2813db3d7212c3e25d
Staritsin <staritsinar@gmail.com>
2025-05-13 21:31:31 +0300

    Update vad_utils.py

--- handlers/vad_utils.py
+++ handlers/vad_utils.py
@@ -1,29 +1,12 @@
 import torch
-import numpy as np
 import torchaudio
-from silero_vad import VoiceActivityDetector
+from silero_vad import get_speech_timestamps, read_audio, save_audio
 
-model, utils = torch.hub.load(
-    repo_or_dir='snakers4/silero-vad',
-    model='silero_vad',
-    trust_repo=True
-)
-
-(get_speech_timestamps, _, read_audio, _, _) = utils
-
-def extract_voice_segments(input_path: str, output_path: str):
+def extract_voice_segments(input_path, output_path):
     wav = read_audio(input_path, sampling_rate=16000)
-    speech_timestamps = get_speech_timestamps(wav, model, sampling_rate=16000)
+    speech_timestamps = get_speech_timestamps(wav, model='silero_vad', sampling_rate=16000)
 
     if not speech_timestamps:
-        raise RuntimeError("⛔ Речь не найдена.")
+        raise ValueError("❌ Речь не найдена — попробуй другое видео")
 
-    # Обрезаем по временным меткам
-    chunks = []
-    for i, t in enumerate(speech_timestamps):
-        start = int(t['start'] * 16)  # 16kHz
-        end = int(t['end'] * 16)
-        chunks.append(wav[start:end])
-
-    speech = torch.cat(chunks)
-    torchaudio.save(output_path, speech.unsqueeze(0), 16000)
+    save_audio(output_path, wav, speech_timestamps, sampling_rate=16000)

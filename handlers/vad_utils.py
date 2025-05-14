6c4bd7a88ea607c1feee7b3f16235d499d13b6f8
Staritsin <staritsinar@gmail.com>
2025-05-14 17:25:47 +0300

    Update vad_utils.py

--- handlers/vad_utils.py
+++ handlers/vad_utils.py
@@ -1,20 +1,21 @@
 import subprocess
 import os
 
+
 def remove_silence(input_path, output_path):
-    subprocess.run([
-        "auto-editor",
-        input_path,
-        "--silent-threshold", "0.03",  # ← это чувствительность тишины
+    try:
+        subprocess.run([
+            "auto-editor",
+            input_path,
+             "--silent-threshold", "0.03",  # ← это чувствительность тишины
         "--frame-margin", "2",         # ← сколько кадров оставить до/после
         "--add-fade", "0.1",           # ← сглаживание в переходах
         "--video-speed", "1",          # ← ускорение/замедление
-        "--export", "video",
-        "--output", output_path
-    ], check=True)
+            "--export", "video",
+            "--output", output_path
+        ], check=True)
 
-
-        return output_path
+        return output_path  # ✅ должен быть на этом уровне, не глубже
 
     except subprocess.CalledProcessError as e:
         print(f"[ERROR] Ошибка удаления тишины: {e}")

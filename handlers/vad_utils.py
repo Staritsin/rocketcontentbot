ff8a72867d7d874eb76c9d0e99dba8aba66635b3
Staritsin <staritsinar@gmail.com>
2025-05-27 17:17:12 +0300

    Update vad_utils.py

--- handlers/vad_utils.py
+++ handlers/vad_utils.py
@@ -8,13 +8,12 @@ def remove_silence(input_path, output_path):
             input_path,
             "--edit", "audio:threshold=3%",
             "--frame_margin", "2",
-            "--video_speed", "1.2",
-            "--output-file", output_path,
-            "--video-codec", "libx264"
+            "--video_speed", "1.1",
+            "--export", "video",
+            "--output-file", output_path
         ], check=True)
-
         return output_path
-
     except subprocess.CalledProcessError as e:
         print(f"[ERROR] Ошибка удаления тишины: {e}")
         return None
+
